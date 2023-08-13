b639ced4ef984c019aabf82ac3f806ebae66b6a0
Ahmed Fathy <ahmedfathy1234553@gmail.com>
2023-08-13 20:10:12 +0300

    add check borders feeature

--- utils.js
+++ utils.js
@@ -7,3 +7,21 @@ function lerp(start, end, t) {
 //lerp(left,right,1/3)->left+1/3(right-left)
 //lerp(left,right,2/3)->left+2/3(right-left)
 //lerp(left,right,3/3)->right
+
+function getIntersection(A, B, C, D) {
+  const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+  const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.x - B.x);
+  const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+  if (bottom != 0) {
+    const t = tTop / bottom;
+    const u = uTop / bottom;
+    if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+      return {
+        x: lerp(A.x, B.x, t),
+        y: lerp(A.y, B.y, t),
+        offset: t
+      };
+    }
+  }
+  return null;
+}
